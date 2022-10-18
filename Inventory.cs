684748b20462f4e41ee3b0cd18ed519dd8d064fc
mani682 <manichandanamiriyala682@gmail.com>
2022-10-18 08:38:02 +0530

    inventory details

---
+++ Inventory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventoryManagement
+{
+    public static class Inventory
+    {
+        public static class InventoryDetails
+        {
+            public TypeOfRice typeOfRice;
+            public TypeOfPulses typeOfPulses;
+            public TypeOfWheat typeOfWheat;
+
+        }
+
+        public class TypeOfRice
+        {
+            public string name { get; set; }
+            public int weight { get; set; }
+            public int price { get; set; }
+
+        }
+        public class TypeOfPulses
+        {
+            public string name { get; set; }
+            public int weight { get; set; }
+            public int price { get; set; }
+
+        }
+        public class TypeOfWheat
+        {
+            public string name { get; set; }
+            public int weight { get; set; }
+            public int price { get; set; }
+
+        }
+    }
+}
