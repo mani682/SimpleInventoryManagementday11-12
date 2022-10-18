89892d16b10730c1c7e39f85c5755ddcd6c536fc
mani682 <manichandanamiriyala682@gmail.com>
2022-10-18 08:36:24 +0530

    inventory details

---
+++ FetchInventoryDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventoryManagement
+{
+    public static class FetchInventoryDetails
+    {
+        public static class FetchForJsonInventoryData
+        {
+            public Inventory Read(string path)
+            {
+                using (StreamReader file = new StreamReader(path))
+                {
+                    try
+                    {
+                        string json = file.ReadToEnd();
+                        return JsonConvert.DeserializeObject<Inventory>(json);
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        return null;
+                    }
+                }
+            }
+        }
+    }
+}
